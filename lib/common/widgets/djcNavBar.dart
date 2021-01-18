78c208208c62b283658d59bb9c10c15c989e6d82
bartonwang <bartonwang@tencent.com>
2021-01-18 23:31:55 +0800

    flutter 屏幕适配方案

---
+++ lib/common/widgets/djcNavBar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DJCNavBar extends StatefulWidget {
+  DJCNavBar(Key key, this.title) : super(key: key);
+  final String title;
+
+  @override
+  _DJCNavBarState createState() {
+    throw _DJCNavBarState();
+  }
+}
+
+class _DJCNavBarState extends State<DJCNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
